0aade53e9fe6b766241577fa33d2b501fe03695c
Faik Hakan Bilgen <faikb@microsoft.com>
2023-01-26 21:22:21 +0300

    Prepared the Outlook Addin Installation Project

--- MeetingCreator/Properties/AssemblyInfo.cs
+++ MeetingCreator/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Security;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("10317ac0-47f3-46c1-aaa1-1be2bcf005a5")]
